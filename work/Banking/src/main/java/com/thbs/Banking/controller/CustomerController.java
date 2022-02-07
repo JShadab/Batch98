517e2e1624bc79a6d6be8cc76ffd3ae2dc50c554
unknown <shadab105662@gmil.com>
2022-02-07 15:35:43 +0530

    Bmking REST

--- work/Banking/src/main/java/com/thbs/Banking/controller/CustomerController.java
+++ work/Banking/src/main/java/com/thbs/Banking/controller/CustomerController.java
@@ -1,26 +1,50 @@
 package com.thbs.Banking.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import java.util.List;
+import java.util.Optional;
 
-@Controller
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.thbs.Banking.entity.Customer;
+import com.thbs.Banking.service.CustomerService;
+
+@RestController
 public class CustomerController {
-	
-	@GetMapping("/register")
-	public String getRegistrationPage() {
-		return "register";
+
+	@Autowired
+	private CustomerService customerService;
+
+	@PostMapping("/customer")
+	public Customer save(@RequestBody Customer customer) {
+		return customerService.save(customer);
 	}
-	
-	@GetMapping("/login")
-	public String getLoginPage() {
-		return "login";
+
+	@PutMapping("/customer")
+	public Customer update(@RequestBody Customer customer) {
+		return customerService.update(customer);
 	}
-	
-	@PostMapping("/login")
-	public String saveXYZ() {
-		return "login";
+
+	@DeleteMapping("/customer/{id}")
+	public void delete(@PathVariable Long id) {
+		customerService.delete(id);
+
 	}
-	
+
+	@GetMapping("/customer/{id}")
+	public Optional<Customer> getOne(@PathVariable Long id) {
+		return customerService.getOne(id);
+	}
+
+	@GetMapping("/customer")
+	public List<Customer> getAll() {
+		return customerService.getAll();
+	}
 
 }
